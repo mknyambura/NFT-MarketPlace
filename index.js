d9ef6fc819778fa32e4ac188a2a1d0ad2a8425d3
mknyambura <kariuki.mercie@gmail.com>
2022-12-14 21:26:06 +0300

    update commit

--- index.js
+++ index.js
@@ -20,4 +20,39 @@ const activeHeaderOnScroll = function(){
         header.classList.remove('active')
     }
 }
-window.addEventListener('scroll', activeHeaderOnScroll)
+window.addEventListener('scroll', activeHeaderOnScroll);
+
+// Slider 
+const slider = document.querySelector('.slider');
+const sliderInitialize = function(currentSlide){
+    const sliderContainer = currentSlide.querySelector('.slider-container');
+    const previousButton = currentSlide.querySelector('[data-slider-previous]')
+    const NextButton = currentSlide.querySelector('[data-slider-next]')
+
+    const totalSlider = Number(getComputedStyle(currentSlide).getPropertyValue('--slider-item'))
+    const sliderItems = sliderContainer.childElementCount.childElementCount - totalSlider
+    
+    const currentSlidePosition = 0;
+    const moveSliderItem = function () {
+        sliderContainer.style.transform = `translateX(-${
+            sliderContainer.children[currentSlidePosition].offsetLeft
+        }px)`;
+    }
+
+    const nextSlide = function (){
+        const endSlide = currentSlidePosition >= totalSlider
+        if (endSlide){
+            currentSlidePosition = 0 ;
+        } else {
+            currentSlidePosition++;
+        }
+        moveSliderItem();
+    }
+
+    NextButton.addEventListener("click", nextSlide);
+}
+
+
+for(const i=0, len=slider.length; i < len; i++) {
+    sliderInitialize(slider[i]);
+}
